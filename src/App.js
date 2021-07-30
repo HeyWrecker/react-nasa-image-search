7e2543416f58224c479e99606f136139a15c9f71
Rick Zawadzki <rzawadzki@crystalcleard.com>
2021-07-30 11:13:42 -0400

    - Imported axios - Added isLoading to state - Added searchResults to state - Disable/enable Button based on isLoading - Add "loading" prop to Search Input based on isLoading - Added async/await call to NASA API to get results and update the state with them.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
@@ -21,19 +22,22 @@ import {
 
 function SearchInput(props) {
     return (
-      <Input fluid
-             icon='images'
-             iconPosition='left'
-             placeholder='Search images...'
-             onInput={(e) => props.onInput(e.target.value)}
-             value={props.value} />
+      <Input  fluid
+              icon='images'
+              iconPosition='left'
+              placeholder='Search images...'
+              onInput={(e) => props.onInput(e.target.value)}
+              value={props.value}
+              loading={props.loading}
+      />
     );
 }
 
 function SearchButton(props) {
     return (
         <Button primary
-          onClick={(e) => props.onClick(e.target.value)}>
+                onClick={(e) => props.onClick(e)}
+                disabled={props.disabled}>
             Search
         </Button>
     );
@@ -43,7 +47,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: ''
+      searchTerm: '',
+      searchResults: [],
+      isLoading: false,
     }
   }
 
@@ -51,18 +57,45 @@ class App extends React.Component {
     const searchTerm = query;
 
     this.setState({
-      searchTerm: searchTerm
+      searchTerm: searchTerm,
     });
   }
 
-  handleClick() {
+  handleClick(e) {
     const searchTerm = this.state.searchTerm;
+    if (searchTerm !== '') {
 
-    alert('Searching for: ' + searchTerm)
+      this.setState({
+        isLoading: true
+      });
+
+      const getResults = async () => {
+        try {
+          const results = await axios.get('https://images-api.nasa.gov/search?q=' + searchTerm);
+
+          if (results.status === 200) {
+            this.setState({
+              searchResults: results.data.collection.items,
+              isLoading: false
+            });
+          } else {
+           /* Handle generic non 200 error TODO */
+          }
+        } catch(error) {
+          console.log('Error:', error)
+        }
+      }
+
+      getResults();
+    } else {
+      alert('Your search must not be empty.');
+    }
   }
 
   render() {
       const currentSearchTerm = this.state.searchTerm;
+      const isLoading = this.state.isLoading;
+
       return (
         <div className="App">
           <Container text>
@@ -73,10 +106,12 @@ class App extends React.Component {
             <Grid columns={2} stackable verticalAlign='middle'>
               <Grid.Column width={12}>
                 <SearchInput value={currentSearchTerm}
-                             onInput={(value) => this.handleInput(value)} />
+                             onInput={(query) => this.handleInput(query)}
+                             loading={isLoading} />
               </Grid.Column>
               <Grid.Column width={4}>
-                <SearchButton onClick={() => this.handleClick()} />
+                <SearchButton onClick={(e) => this.handleClick(e)}
+                              disabled={isLoading} />
               </Grid.Column>
             </Grid>
           </Container>
